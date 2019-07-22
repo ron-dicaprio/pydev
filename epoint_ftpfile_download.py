7e5583419754e66129d32f8140a61ce99e661fe8
ron-dicaprio <love6018@hotmail.com>
2019-07-22 22:54:15 +0800

    Update epoint_ftpfile_download.py

--- epoint_ftpfile_download.py
+++ epoint_ftpfile_download.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 #@author epoint_caitao
+#@version python 3.7.1rc1
 #from bs4 import BeautifulSoup as soup
 import re
 import requests
@@ -12,20 +13,20 @@ url = 'https://oa.epoint.com.cn/EpointTrainingManage/TrainingFile/File/'
 response = requests.get(url,headers = chorme_header)
 response.encoding='utf-8'
 target = re.findall(r'A HREF="(.*?)"',response.text)
-file_no = 0
+filename_no = 0
 for i in range(1,len(target)):
     url1 = 'https://oa.epoint.com.cn' + target[i]
     response1 = requests.get(url1,headers = chorme_header)
     response1.enoding ='utf-8'
     target1 = re.findall(r'<A HREF="(.*?)">',response1.text)
     url2 = 'https://oa.epoint.com.cn' + target1[1]
-    print('url2:',url2)
+    #print('url2:',url2)
     #strsql = "insert into pyurl set pyepurl = '%s'" % (url2)
     #cur.execute(strsql)
     #conn.commit()
     response2 =requests.get(url2,headers = chorme_header)
-    file_no += 1
-    with open('E:\\pydownload\\%s.%s' % (file_no,target1[1].split('.')[-1]),'wb+') as file:
+    filename_no += 1
+    with open('E:\\pydownload\\%s.%s' % (filename_no,target1[1].split('.')[-1]),'wb+') as file:
         file.write(response2.content)
         print(i,'file saved,url is ',url2)
         file.close()
