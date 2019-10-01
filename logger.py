bbe403eee1e465f783a31fab1ff7a9c486bde38b
ron-dicaprio <love6018@hotmail.com>
2019-10-01 23:27:55 +0800

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,3 +1,30 @@
+# -*- encoding:utf-8 -*-
+import os
+import datetime
+if os.path.exists('logs'):
+    pass
+else:
+    os.mkdir('logs')
+'''
+#another kind of time format 
+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+'''
+def info(self):
+    file = open('logs/%s.wrapper' % (datetime.datetime.now().date()),'a+',encoding='utf-8')
+    file.writelines('###  %s |info| %s\n' % (datetime.datetime.now(),str(self)))
+    file.close()
+    
+def warnnig(self):
+    file = open('logs/%s.wrapper' % (datetime.datetime.now().date()),'a+',encoding='utf-8')
+    file.writelines('###  %s |warn| %s\n' % (datetime.datetime.now(),str(self)))
+    file.close()
+    
+def error(self):
+    file = open('logs/%s.wrapper' % (datetime.datetime.now().date()),'a+',encoding='utf-8')
+    file.writelines('###  %s |erro| %s\n' % (datetime.datetime.now(),str(self)))
+    file.close()
+'''
+#i do not like this kind of expression
 import time
 import os
 if os.path.exists('d:\\spy'):
@@ -11,3 +38,4 @@ class pylog(object):
         file = open('d:\\spy\\loginfo_%s.wrapper' % (filenamedate),'a+',encoding='utf-8')
         file.writelines(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) +'  '+ str(self) + '\n')
         file.close()
+'''
