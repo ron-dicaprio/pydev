6297ffd4e56c6b364658b7fdc5bb396d6a17fcab
ron-dicaprio <love6018@hotmail.com>
2021-07-05 19:29:16 +0800

    Update py_email.py

--- py_email.py
+++ py_email.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin env python
 import smtplib
-import datetime
 import time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -13,24 +12,28 @@ def py_email(subject,message_con,filepaths,to_user):
     mail_server = smtplib.SMTP('smtp.qq.com',25)
     mail_server.login(from_user,from_passwd)
     filepaths = filepaths.replace('/','\\')
-    print(filepaths)
     #message = MIMEText(message_con, 'plain', 'utf-8')
-    #message = MIMEMultipart()
     #构建一个带附件的实例
     message = MIMEMultipart()
     message['Subject'] = subject
     message['From'] = from_user
-    message['To'] = ','.join(to_user)
+    message['To'] = ';'.join(to_user)
     message.attach(MIMEText(message_con, 'plain', 'utf-8'))
     for filenames in (filepaths.split(';')):
-        print(filenames)
         attachfile =  MIMEText(open(filenames, 'rb').read(), 'base64', 'utf-8')
         attachfile["Content-Type"] = 'application/octet-stream'
         attachfile["Content-Disposition"] = 'attachment; filename=%s' % (str(filenames).split('\\')[-1])
         message.attach(attachfile)
     try:
+        starttime = time.time()
+
         mail_server.sendmail(from_user, message['To'].split(';'), message.as_string())
         print('email send success!')
+        endtime = time.time()
+
+        print('email cost',endtime-starttime,'secends')
     except Exception as e:
         print(e)
     mail_server.quit()
+if __name__ == '__main__':
+    py_email(subject, message_con, filepaths, to_user)
